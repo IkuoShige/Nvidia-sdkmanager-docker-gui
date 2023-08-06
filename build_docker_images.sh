07e3f0d793c15cc6144e8c6f6f3d74176ee86e37
IkuoShige <shigeikuo@gmail.com>
2023-08-06 10:15:21 +0900

    initial commit

---
+++ build_docker_images.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# usage
+# 使用方法
+# ./build_docker_images.sh --ubuntu-version 20.04 --docker-version 1.9.3.10904
+
+# ヘルプメッセージを表示する関数
+# Function to display help messages
+function show_help {
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  -h, --help               Show this help message and exit"
+    echo "  --ubuntu18               Use Ubuntu 18.04 as the base image"
+    echo "  --docker-version         Set the sdkmanager docker version. Pick up <sdkmanager-docker-version> in the sdkmanager-<sdkmanager-docker-version>-ubuntu_<distr>_docker.tar.gz"
+    echo "  --uid                    Set the UID for the user in the container"
+    echo "  --gid                    Set the GID for the user in the container"
+}
+
+# デフォルトのオプション
+# Default options
+DISTR="20.04"
+USER_UID=$(id -u)
+USER_GID=$(id -g)
+SDK_MANAGER_VERSION=""
+SDK_MANAGER_DOCKER_VERSION=""
+DEB_FILE=""
+
+# コマンドライン引数を解析
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            show_help
+            exit 0
+            ;;
+        --ubuntu-distr)
+            DISTR="$2"
+            shift
+            ;;
+        --uid)
+            USER_UID="$2"
+            shift 2
+            ;;
+        --gid)
+            USER_GID="$2"
+            shift 2
+            ;;
+        --docker-version)
+            SDK_MANAGER_DOCKER_VERSION="$2"
+            shift 2
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+# ビルド実行
+# Execute the build process
+echo "Building Docker image with base image: ${DISTR}, SDK Manager version: ${SDK_MANAGER_VERSION}"
+docker build \
+    --build-arg SDK_MANAGER_VERSION=${SDK_MANAGER_VERSION} \
+    --build-arg SDK_MANAGER_DOCKER_VERSION=${SDK_MANAGER_DOCKER_VERSION} \
+    --build-arg UID=${USER_UID} \
+    --build-arg GID=${USER_GID} \
+    --build-arg DISTR=${DISTR} \
+    -t sdkmanager_gui_${DISTR} \
+    -f Dockerfile.latest \
+    .
+
+
+echo "Done."
---
+++ launch_container.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+# Default JETPACK_HOME path
+# デフォルトのJETPACK_HOMEパス
+DEFAULT_JETPACK_HOME=$(realpath ./jetpack_home)
+JETPACK_HOME=$DEFAULT_JETPACK_HOME
+
+# Default distribution
+# デフォルトのディストリビューション
+DISTR="20.04"
+while [ "$#" -gt 0 ]; do
+    case $1 in
+        --ubuntu-version)
+            # If the --ubuntu-version option is given, set the next argument as DISTR
+            # --ubuntu-version オプションが指定された場合、次の引数をDISTRに設定
+            DISTR="$2"
+            shift
+            ;;
+        --jetpack-home)
+            # If the --jetpack-home option is given, set the next argument as JETPACK_HOME
+            # --jetpack-home オプションが指定された場合、次の引数をJETPACK_HOMEに設定
+            JETPACK_HOME=$(realpath $2)
+            shift 2
+            ;;
+        *)
+            exit 1
+            ;;
+    esac
+done
+
+docker run --privileged --rm -it \
+           --volume=$XSOCK:$XSOCK:rw \
+           --volume=$XAUTH:$XAUTH:rw \
+           --volume="$JETPACK_HOME/nvidia":/home/nvidia/nvidia:rw \
+           --volume="$JETPACK_HOME/Downloads":/home/nvidia/Downloads:rw \
+           -v /dev/bus/usb:/dev/bus/usb/ \
+           --shm-size=1gb \
+           --env="XAUTHORITY=${XAUTH}" \
+           --env="DISPLAY=${DISPLAY}" \
+           --env=TERM=xterm-256color \
+           --env=QT_X11_NO_MITSHM=1 \
+           --net=host \
+           -u "nvidia"  \
+           sdkmanager_gui_${DISTR}:latest
+
